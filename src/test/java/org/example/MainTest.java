551dd04432fcc0c90f6f8ddf1a0223331af50c3c
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-08-24 15:19:37 +0200

    feat: test cases extended

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,10 +5,15 @@ import org.junit.jupiter.api.Test;
 
 class MainTest {
 
-    @Test void return0_whenFibonacci_gets0() { testACase(0, 0); }
-    @Test void return1_whenFibonacci_gets1() { testACase(1, 1); }
-    @Test void return1_whenFibonacci_gets2() { testACase(2, 1); }
-    @Test void return2_whenFibonacci_gets3() { testACase(3, 2); }
+    @Test void returnMin3_whenFibonacci_getsMin4() { testACase(-4, -3); }
+    @Test void return2____whenFibonacci_getsMin3() { testACase(-3,  2); }
+    @Test void returnMin1_whenFibonacci_getsMin2() { testACase(-2, -1); }
+    @Test void return1____whenFibonacci_getsMin1() { testACase(-1,  1); }
+    @Test void return0____whenFibonacci_gets0   () { testACase( 0,  0); }
+    @Test void return1____whenFibonacci_gets1   () { testACase( 1,  1); }
+    @Test void return1____whenFibonacci_gets2   () { testACase( 2,  1); }
+    @Test void return2____whenFibonacci_gets3   () { testACase( 3,  2); }
+    @Test void return2____whenFibonacci_gets4   () { testACase( 4,  3); }
 
     void testACase(int n, int expected) {
         //GIVEN
